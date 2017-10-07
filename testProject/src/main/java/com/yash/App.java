987a4428804b4b2cf3300f31a64d45056019164a
yashpal <yashpaldhingra@yahoo.com>
2017-10-08 00:42:43 +0530

    mongodb

--- testProject/src/main/java/com/yash/App.java
+++ testProject/src/main/java/com/yash/App.java
@@ -1,9 +1,14 @@
 package com.yash;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.data.mongodb.core.MongoOperations;
 
 import com.yash.domain.Application;
+import com.yash.model.Car;
 
 public class App 
 {
@@ -11,11 +16,18 @@ public class App
     {
     	 String confFile = "spring.xml";
          
-    	 ConfigurableApplicationContext context 
-         = new ClassPathXmlApplicationContext(confFile);
+    	 ApplicationContext context = new GenericXmlApplicationContext(confFile);
+    	 MongoOperations mongoOperation = (MongoOperations)context.getBean("mongoTemplate");
 Application dbConfig = (Application) context.getBean("appDetail");
-System.out.println(dbConfig.toString());
-context.close();
-     
+Car car = new Car();
+car.setMake("2010");
+car.setDescription("desc");
+car.setModel("dezire");
+
+
+// save
+mongoOperation.save(car);
+
+System.out.println(dbConfig.toString() );    
     }
 }
---
+++ testProject/src/main/java/com/yash/repository/CarMongoRepository.java
@@ -0,0 +1,7 @@
+package com.yash.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.yash.model.Car;
+
+public interface CarMongoRepository extends CrudRepository{}
